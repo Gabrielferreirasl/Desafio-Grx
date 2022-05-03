c5d675a7aa8a69d7a9b7fdd651a147285410480a
Gabriel <gabrielferreiradsl@hotmail.com>
2022-05-03 20:45:12 -0300

    feat: pages -> adiciona a Home e a estrutura inicial

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function Home() {
+  const [questions, setQuestion] = useState({
+    questionOne: '',
+    questionTwo: '',
+    questionThree: 'sim',
+    questionFour: '',
+  });
+
+  return (
+    <p>oi</p>
+  )
+}
+
+export default Home;
