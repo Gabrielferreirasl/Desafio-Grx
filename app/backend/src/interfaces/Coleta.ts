ab77e2c7009a581f46dd21ce8bb83c50ff3e5bb2
Gabriel <gabrielferreiradsl@hotmail.com>
2022-05-04 13:40:18 -0300

    feat: adição das interfaces

---
+++ app/backend/src/interfaces/Coleta.ts
@@ -0,0 +1,12 @@
+export interface Answers {
+  Pergunta1: string,
+  Pergunta2: string,
+  Pergunta3: string,
+  Pergunta4: string
+}
+
+export interface Data {
+  QuantidadePositiva: number,
+  QuantidadeNegativa: number,
+  QuantidadeNaoAvaliada: number
+}
