bcb4002f66a3ce2f0294b9ecac72f62234f1c74b
Gabriel <gabrielferreiradsl@hotmail.com>
2022-05-03 21:04:26 -0300

    feat: pages -> ultilização do component Question e adição da função handleForm

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Question from "../componets/Question";
 
 function Home() {
   const [questions, setQuestion] = useState({
@@ -8,8 +9,31 @@ function Home() {
     questionFour: '',
   });
 
+  const handleForm = ({ target: { name, value } }) => {
+    setQuestion((prev) => ({
+      ...prev,
+      [name]: value,
+    }))
+  }
+
   return (
-    <p>oi</p>
+    <>
+    <header>
+      <h1>Desafio Coleta - GRX</h1>
+    </header>
+    <main>
+      <Question 
+      question="1 - Você se considera bom em lógica?"
+      name="questionOne"
+      onClickFunction={ handleForm }
+      />
+      <Question 
+      question="2 - Gosta de aprender com desafios?"
+      name="questionTwo"
+      onClickFunction={ handleForm }
+      />
+    </main>
+    </>
   )
 }
 
